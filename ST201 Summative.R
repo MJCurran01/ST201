fd4df29325ac9f27a7de75c840480dc3f612a1bc
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2023-04-29 01:13:06 +0100

    Added forward and backwards feature selection

--- ST201 Summative.R
+++ ST201 Summative.R
@@ -13,20 +13,132 @@
 
 rm(list = ls()) # clear workspace
 setwd("") # setting working directory
+gc()            # Clear memory
+cat("\f")       # Clear the console
+options(scipen = 9) # Remove scientific notation for numbers with 9 decimals or less
 
 ######## PACKAGES ########
-
-# Check system and installs packages user doesn't have, load needed packages
-
-need <- c("dplyr") # list packages needed
-have <- need %in% rownames(installed.packages()) # checks packages you have
-if(any(!have)) install.packages(need[!have]) # install missing packages
-invisible(lapply(need, library, character.only=T)) # load needed packages
-
+# Prepare needed libraries
+packages <- c("haven" # To import *.dta files
+              , "stargazer" # Nice output tables
+              , "MASS" # For stepwise selection
+              , "ISLR"
+              , "glmnet"
+              , "boot"
+              , "leaps"
+)
+for (i in 1:length(packages)) {
+  if (!packages[i] %in% rownames(installed.packages())) {
+    install.packages(packages[i]
+                     , repos = "http://cran.rstudio.com/"
+                     , dependencies = TRUE
+    )
+  }
+  library(packages[i], character.only = TRUE)
+}
+rm(packages)
 
 # Attaching data
 data <- read.csv("coviddata.csv")
-data <- na.omit(data)
+data <- na.omit(data)[,-1] #omit NAs and the index column
+data$Gad_score <- scale(data$Gad_score) #normalize data
+
+# try simple OLS first and obtain summary
+covid.simple.OLS <- lm(Gad_score~., covid.train)
+summary(covid.simple.OLS)
+
+#save results into a data frame
+results.ols <- data.frame(feature = names(covid.best.OLS$coefficients)[-1] # -1 to exclude intercept
+                          , ols = round(coef(covid.best.OLS)[-1], 5) # -1 to exclude intercept
+)
+
+for (i in 1:ncol(data)){
+  if (!(colnames(data)[i] %in% c('Age', 'Social_support', 'Covid_risk', 'Gad_score'))){
+    data[, colnames(data)[i]] <- as.factor(data[, colnames(data)[i]])
+  }
+}
+
+#train-test
+set.seed(1000)
+split <- sample(c(TRUE, FALSE), nrow(data), replace=TRUE, prob=c(0.75,0.25))
+covid.train <- data[split,]
+covid.test <- data[!split,]
+
+# try OLS first and obtain summary
+covid.best.OLS <- lm(Gad_score~., covid.train)
+summary(covid.best.OLS)
+
+#save results into a data frame
+results.ols <- data.frame(feature = names(covid.best.OLS$coefficients)[-1] # -1 to exclude intercept
+                          , ols = round(coef(covid.best.OLS)[-1], 5) # -1 to exclude intercept
+)
+
+covid.stepwise <- data.frame(model.matrix(Gad_score ~ ., covid.train)[,-1])
+covid.stepwise$Gad_score <- covid.train$Gad_score
+
+# try step wise selection
+covid.forward <- regsubsets(Gad_score~., covid.stepwise, 
+                                  nvmax=NULL, method="forward"
+)
+covid.backward <- regsubsets(Gad_score~., covid.stepwise, 
+                                 nvmax=NULL, method="backward"
+)
+
+get_formula <- function(nfeatures, reg, outcome){
+  vars <- summary(reg)$which[nfeatures,-1]
+  predictors <- names(which(vars==TRUE))
+  predictors <- paste(predictors, collapse = " + ")
+  formula <- as.formula(paste0(outcome, " ~ ", predictors))
+  return(formula)
+}
+
+cv_best_features <- function(reg){
+  cv.fit <- data.frame(features = seq.int(from = 1, to = ncol(summary(reg)$which)-1, by = 1),
+                       mse = NA
+  )
+  for (i in 1:(ncol(summary(reg)$which)-1)) { 
+    # The inner loop cycles through number of features
+    formula.cv <- get_formula(i, reg, 'Gad_score')
+    # Estimate the model
+    model.cv <- glm(as.formula(formula.cv), data = covid.stepwise)
+    #  Calculate MSE from CV
+    set.seed(1000)
+    mse <- cv.glm(data = covid.stepwise, glmfit = model.cv, K = 10)$delta[1]
+    # Put MSE back into main dataframe
+    cv.fit$mse[cv.fit$features == i] <- mse
+  }
+  return(cv.fit[which.min(cv.fit$mse),]$features)
+}
+
+cv_best_features(covid.forward)
+#14
+cv_best_features(covid.backward)
+#14
+
+#set up 
+covid.best.forward <- lm(get_formula(24, covid.forward, 'Gad_score'), data=covid.stepwise)
+covid.best.backward <- lm(get_formula(24, covid.backward, 'Gad_score'), data=covid.stepwise)
+
+
+#generate predictions
+prediction <- data.frame(Gad_score = covid.test$Gad_score)
+
+covid.stepwise.test <- data.frame(model.matrix(Gad_score ~ ., covid.test)[,-1])
+covid.stepwise.test$Gad_score <- covid.test$Gad_score 
+
+prediction$score.OLS <- predict(covid.best.OLS, newdata = as.data.frame(covid.test))
+prediction$score.forward <- predict(covid.best.forward, newdata = as.data.frame(covid.stepwise.test))
+prediction$score.backward <- predict(covid.best.backward, newdata = as.data.frame(covid.stepwise.test))
+
+#Calculate MSE
+accuracy <- data.frame(model = c('OLS(nonfactor)', 'OLS', 'forward', 'backward'), MSE=NA)
+
+accuracy$MSE[accuracy$model == "OLS"] <- mean((prediction$Gad_score - prediction$score.OLS)^2)
+accuracy$MSE[accuracy$model == "forward"] <- mean((prediction$Gad_score - prediction$score.forward)^2)
+accuracy$MSE[accuracy$model == "backward"] <- mean((prediction$Gad_score - prediction$score.backward)^2)
+accuracy
+
+#-----------------------------------------------------------------------------------------
 
 # Create the design matrix
 X <- as.matrix(data[, c("Sex", "Age", "Education", "IncomeContinuity", "HealthStatus", "Unemployed", "Student",
